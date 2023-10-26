ce166598c4059a52267e3268f5fd1bdfb57a6cf7
ashokkumar6384 <149041522+ashokkumar6384@users.noreply.github.com>
2023-10-26 12:24:24 +0530

    Add files via upload

---
+++ ashok.php
@@ -0,0 +1,53 @@
+<style>
+img{
+	width: 100px;
+	height: 100PX;
+	border: 2px solid #white;
+}
+</style>
+<DOCTYPE html>
+	<html>
+<head>
+	<br><br><br>
+
+
+<font color="skyblue">
+	<br><br><br>
+
+	<center><h1><b>USER LOGIN</b></h1><br>
+		<center><h2><b><u>Welcome to Intranet Chat</u> </b></h2>
+			<body>
+	<img src="http://localhost/MARISH/project/assets/08.jpg">
+	
+	
+		<h4>sign in now to chat and file transfer all in one place.</h4></font>
+		
+<font color="white" name="usrname">
+Username
+<input type="text" name="usrname" placeholder="Enter your name"><br><br>
+Password<font color="skyblue" name="password">
+<input type="password" name="usrname"placeholder="Enter your password"><br><br>
+
+<input type="submit" name="submit" value="Login"><a href="http://localhost/MARISH/project/Login.php"></a><br><br>
+
+
+
+
+</center>
+
+
+
+
+<style>
+	body {
+		background-image: url("http://localhost/MARISH/project/assets/fg.jpg");
+	/*background-color: skyblue;*/
+    
+
+	}
+
+</style>
+</head>
+
+</body>
+</html>
